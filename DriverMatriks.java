89d91b9826660c0dfb938af4b580ebe9d5cbe700
Rhapsodya Piedro Asmorobangun <piedrorhapsodya@gmail.com>
2020-09-23 23:55:22 +0700

    Gauss

--- DriverMatriks.java
+++ DriverMatriks.java
@@ -2,29 +2,46 @@ class DriverMatriks {
     public static void main(String[] args) {
         matriks M = new matriks();
 
-        M.isimatriks(4,4);
+        // M.isimatriks(4,4);
 
-        System.out.println("Isi matriks: ");
-        M.tulismatriks(4,4);
+        // System.out.println("Isi matriks: ");
+        // M.tulismatriks(4,4);
 
-        int [][] M2 = new int[4][4];
-        int i, j;
-        for (i=0;i<4;i++) {
-            for (j=0;j<4;j++) {
-                M2[i][j] = M.get(M,i,j);
-            }
-        }
+        // int [][] M2 = new int[4][4];
+        // for (i=0;i<4;i++) {
+        //     for (j=0;j<4;j++) {
+        //         M2[i][j] = M.get(M,i,j);
+        //     }
+        // }
         // M.transpose(4,3,M2);
+        // System.out.print("Determinan matriks: ");
+        // System.out.println(M.determinan(4,M2));
+
+        float [][] M3 = {{2, 3, -1, 5}, 
+                        {4, 4, -3, 3}, 
+                        {-2, 3, -1, 1}};
 
         // System.out.println("Isi matriks transpose: ");
-        // int i, j;
         // for (i=0;i<4;i++) {
-        //     for (j=0;j<3;j++) {
-        //         System.out.print(M2[i][j] + " ");
+        //     for (j=0;j<4;j++) {
+        //         System.out.print(M3[i][j]);
+        //         if (j < 3)
+        //             System.out.print(" ");
         //     }
         //     System.out.println();
         // }
-        System.out.print("Determinan matriks: ");
-        System.out.println(M.determinan(4,M2));
+
+        M.gauss(3,4,M3);
+
+        System.out.println("Isi matriks transpose: ");
+        int i, j;
+        for (i=0;i<3;i++) {
+            for (j=0;j<4;j++) {
+                System.out.print(M3[i][j]);
+                if (j < 3)
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
--- matriks.java
+++ matriks.java
@@ -7,9 +7,8 @@ public class matriks {
         int i, j;
 
         for (i=0;i<10;i++) {
-            for (j=0;j<10;j++) {
+            for (j=0;j<10;j++)
                 this.Mat[i][j] = 0;
-            }
         }
     }
 
@@ -21,9 +20,8 @@ public class matriks {
         int i, j;
 
         for (i=0;i<N;i++) {
-            for (j=0;j<M;j++) {
+            for (j=0;j<M;j++)
                 this.Mat[i][j] = i+j;
-            }
         }
     }
 
@@ -32,7 +30,9 @@ public class matriks {
 
         for (i=0;i<N;i++) {
             for (j=0;j<M;j++) {
-                System.out.print(this.Mat[i][j] + " ");
+                System.out.print(this.Mat[i][j]);
+                if (j < M - 1)
+                    System.out.print(" ");
             }
             System.out.println();
         }
@@ -42,13 +42,12 @@ public class matriks {
         int i, j;
 
         for (i=0;i<N;i++) {
-            for (j=0;j<M;j++) {
+            for (j=0;j<M;j++)
                 Matt[i][j] = this.Mat[j][i];
-            }
         }
     }
 
-    float determinan(int N, int Mat[][]) {
+    float determinan(int N, int M[][]) {
         int i, j, k;
         float D = 0;
         if (N!=2) {
@@ -58,25 +57,38 @@ public class matriks {
                     for (j = 0; j < N; ++j) {
                         if (k!=j) {
                             if (j<k)
-                                K[i-1][j] = Mat[i][j];
+                                K[i-1][j] = M[i][j];
                             else
-                                K[i-1][j-1] = Mat[i][j];
+                                K[i-1][j-1] = M[i][j];
                         }
                     }
                 }
-                System.out.print("D awal: ");
-                System.out.println(D);
                 if (k%2 == 0)
-                    D = D + (Mat[0][k] * determinan(N-1,K));
+                    D = D + (M[0][k] * determinan(N-1,K));
                 else
-                    D = D - (Mat[0][k] * determinan(N-1,K));
-                System.out.print("D akhir: ");
-                System.out.println(D);
+                    D = D - (M[0][k] * determinan(N-1,K));
             }
         }
         else
-            D = (Mat[0][0] * Mat[1][1]) - (Mat[0][1] * Mat[1][0]);
+            D = (M[0][0] * M[1][1]) - (M[0][1] * M[1][0]);
         return D;
+    }
+
+    void gauss(int M, int N, float G[][]) {
+        int i, j, k, l;
+        float div, mul;
+        for ( i = 0; i < M; ++i) {
+            div = G[i][i];
+            for (j = i; j < N; ++j)
+                G[i][j] = G[i][j] / div;
+            if (i < (M - 1)) {
+                for ( k = i + 1; k < M; ++k) {
+                    mul = G[k][i] / G[i][i];
+                    for (l = 0; l < N; ++l)
+                        G[k][l] = G[k][l] - (mul*G[i][l]);
+                }
+            }
+        }
     }
 
 }
