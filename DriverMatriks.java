bc2e0fa3047275ee695df97e3a7121f647e8327c
Rhapsodya Piedro Asmorobangun <piedrorhapsodya@gmail.com>
2020-09-23 18:38:02 +0700

    Determinan Matriks

--- DriverMatriks.java
+++ DriverMatriks.java
@@ -2,9 +2,29 @@ class DriverMatriks {
     public static void main(String[] args) {
         matriks M = new matriks();
 
-        M.isimatriks(3,4);
+        M.isimatriks(4,4);
 
         System.out.println("Isi matriks: ");
-        M.tulismatriks(3,4);
+        M.tulismatriks(4,4);
+
+        int [][] M2 = new int[4][4];
+        int i, j;
+        for (i=0;i<4;i++) {
+            for (j=0;j<4;j++) {
+                M2[i][j] = M.get(M,i,j);
+            }
+        }
+        // M.transpose(4,3,M2);
+
+        // System.out.println("Isi matriks transpose: ");
+        // int i, j;
+        // for (i=0;i<4;i++) {
+        //     for (j=0;j<3;j++) {
+        //         System.out.print(M2[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        System.out.print("Determinan matriks: ");
+        System.out.println(M.determinan(4,M2));
     }
 }
--- matriks.java
+++ matriks.java
@@ -13,6 +13,10 @@ public class matriks {
         }
     }
 
+    int get(matriks M, int i, int j) {
+        return this.Mat[i][j];
+    }
+
     void isimatriks(int N, int M) {
         int i, j;
 
@@ -34,13 +38,45 @@ public class matriks {
         }
     }
 
-    void transpose(int N, int M, int Mat[][]) {
+    void transpose(int N, int M, int Matt[][]) {
         int i, j;
 
         for (i=0;i<N;i++) {
             for (j=0;j<M;j++) {
-                Mat[j][i] = Mat[i][j];
+                Matt[i][j] = this.Mat[j][i];
             }
         }
     }
+
+    float determinan(int N, int Mat[][]) {
+        int i, j, k;
+        float D = 0;
+        if (N!=2) {
+            for (k = 0; k < N; ++k) {
+                int [][] K = new int[N-1][N-1];
+                for ( i = 1; i < N; ++i) {
+                    for (j = 0; j < N; ++j) {
+                        if (k!=j) {
+                            if (j<k)
+                                K[i-1][j] = Mat[i][j];
+                            else
+                                K[i-1][j-1] = Mat[i][j];
+                        }
+                    }
+                }
+                System.out.print("D awal: ");
+                System.out.println(D);
+                if (k%2 == 0)
+                    D = D + (Mat[0][k] * determinan(N-1,K));
+                else
+                    D = D - (Mat[0][k] * determinan(N-1,K));
+                System.out.print("D akhir: ");
+                System.out.println(D);
+            }
+        }
+        else
+            D = (Mat[0][0] * Mat[1][1]) - (Mat[0][1] * Mat[1][0]);
+        return D;
+    }
+
 }
