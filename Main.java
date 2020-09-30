bb84e8c6441b846376b104f7916ebf1c504dae65
Aditya Bimawan <aditditto@gmail.com>
2020-09-30 21:07:10 +0700

    Bikin method inversSPL di MAugmented

--- Main.java
+++ Main.java
@@ -1,23 +1,26 @@
 public class Main {
-/*
-            NOTE LEO: kayaknya kita harus misahin matriks A dan 
-                      matriks b di Ax=b
-*/
     public static void main(String[] args) {
-        Matriks M = new Matriks(new float[][] { {3, 5, 6},
-                                                {7 ,4, 8},
-                                                {6, 5, 1}});
+        Matriks a = new Matriks(new float[][] {{1, 3, 3}, {3, 4, 5}});
+        Matriks b = new Matriks(new float[][] {{3}, {2}, {5}});
+        
+        System.out.println(a.kaliMatriks(b));
+        // Matriks M = new Matriks(new float[][] { {3, 5, 6},
+        //                                         {7 ,4, 8},
+        //                                         {6, 5, 1}});
 
-        Matriks augmented = new Matriks(new float[][] { {1, 1, 1, 1},
+        MAugmented augmented = new MAugmented(new Matriks(new float[][] { {1, 1, 1, 1},
                                                         {1, 2, 4, 2},
-                                                        {1, 3, 9, 3} });
+                                                        {1, 3, 9, 3} }));
+
+        System.out.println(augmented.gaussjor());
+        System.out.println(augmented.inversSPL());
         
         
-        MAugmented augmented2 = new MAugmented (augmented);
-        System.out.println(augmented2.MatKoef);
-        System.out.println(augmented2.MatVal);
-        Matriks Mgauss = augmented.gauss();
-        System.out.println(Mgauss);
+        // MAugmented augmented2 = new MAugmented (augmented);
+        // System.out.println(augmented2.MatKoef);
+        // System.out.println(augmented2.MatVal);
+        // Matriks Mgauss = augmented.gauss();
+        // System.out.println(Mgauss);
         //System.out.println(M);
         //System.out.println("determinan: " + M.determinan());
 
@@ -26,7 +29,7 @@ public class Main {
         // augmented.gauss();
         // System.out.println(augmented);
         
-        Interpolasi X=new Interpolasi();
+        // Interpolasi X=new Interpolasi();
 
 
         
